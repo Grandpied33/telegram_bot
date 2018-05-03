19a03f2eb78dc7d27c6c65e6f216df5bc09f2ecd
hugomonfouga <hugo.monfouga@ynov.com>
2018-05-03 13:23:59 +0200

    MAJ du ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # telegram_bot
+
+
+Ce super bot trop cool et trop simple à la fois va te faire kiffer la METEEOOOOOH !!
+
+Pour utiliser le bot
+
+Installer TelegramBot :
+
+`npm install node-telegram-bot-api`
+
+Installer Q :
+
+`npm install q`
+
+Une fois cela fait il se peut que vous ayez à créer votre propore Bot telegram.
+Rien de plus simple.
+ 
+Sur télégram contacter BotFather : https://telegram.me/BotFather
+Une fois que vous l'avez contacter écrire : `/newbot`
+
+Il va vous poser une ou 2 questions et vous aurez une message comme suivant :
+
+`Done! Congratulations on your new bot. You will find it at _Votre lien_. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+ 
+ Use this token to access the HTTP API:
+ _Votre Token_
+ 
+ For a description of the Bot API, see this page: https://core.telegram.org/bots/api`
+ 
+ 
+ 
+ Une fois cela fais le metttez votre token dans le code 
+
+
+
--- bot.js
+++ bot.js
@@ -2,39 +2,42 @@ var TelegramBot = require('node-telegram-bot-api');
 var token = '523946695:AAHLgLMtqSDQgjflsxKPwpBV6P7lHHZouh8';
 var Q = require('q');
 var request = Q.denodeify(require("request"));
-
+//var player = require('https://kaamelott-soundboard.2ec0b4.fr/sounds/meteo.mp3')(opts = {});
+//var audio = new Audio('https://kaamelott-soundboard.2ec0b4.fr/sounds/meteo.mp3');
 
 var bot = new TelegramBot(token, {polling: true});
 bot.getMe().then(function (me) {
-    console.log('Salut je mon nom est : %s!', me.username);
+    console.log('Wesh, moi c\'est %s!', me.username);
 });
 
 bot.onText(/\/start/, function (msg, match) {
     var fromId = msg.from.id; // get the id, of who is sending the message
-    var message = "Bienvenue dans votre bot Meteo\n"
-    message += "Obtenez la méteo via la commande /weather [la_ville]."
+    var message = "Bienvenue sur MeteoBot\n"
+    message += "Obtenez la méteo en entrant la command '/meteo [code_postal]'."
     bot.sendMessage(fromId, message);
+
 });
 
 // match /weather [whatever]
-bot.onText(/\/weather (.+)/, function (msg, match) {
+bot.onText(/\/meteo (.+)/, function (msg, match) {
     var fromId = msg.from.id; // get the id, of who is sending the message
     var postcode = match[1];
     getWeatherData(postcode)
         .then(function(data){
-            var message = "Voilà la météo à "+postcode+"\n";
+            var message = "La méteo d'aujourd'hui dans le "+postcode+"\n";
             message += data.wx_desc+"\n"
-            message += "temp: "+data.temp_c+"C or "+data.temp_f+"F"
+            message += "température: "+data.temp_c
             bot.sendMessage(fromId, message);
         });
 
 });
 
+
 function getWeatherData(postcode){
     var app_id = "b59cf63e"
     var app_key = "2bd0242e44b87eb875660ad9fbde90e5"
-    var url = "http://api.weatherunlocked.com/api/current/us."+postcode
-    url += "?app_id="+app_id+"&app_key="+app_key
+    var url = "http://api.weatherunlocked.com/api/current/fr."+postcode
+    url += "?lang=fr&app_id="+app_id+"&app_key="+app_key
 
     var options ={
         url: url,
